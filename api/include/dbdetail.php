15c5de4bdf553c0c5579010294673cc7f59d3b8e
lzq98 <1170591186@qq.com>
2023-07-12 18:42:30 +0930

    Ignore db detail chage

--- api/include/dbdetail.php
+++ api/include/dbdetail.php
@@ -1,7 +1,7 @@
 <?php
 // update this file according to your db environment
 $servername = "127.0.0.1";
-$username = "your_db_username";
-$password = "your_db_password";
-$dbname = "your_db_name";
+$username = "cschat";
+$password = "cschat123";
+$dbname = "cschat";
 ?>
