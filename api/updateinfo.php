fd8718f337f7ad02f006e47b29dbed280abb2084
lzq98 <1170591186@qq.com>
2023-07-30 17:24:05 +0930

    Revert "Bug fix on changing profile"
    This reverts commit 8626de1237173ee8064c548f180d8a7b218b6fae.

--- api/updateinfo.php
+++ api/updateinfo.php
@@ -29,44 +29,34 @@ $name = htmlentities($name);
 $email = strtolower(htmlentities($email));
 
 $query = sprintf(
-    "SELECT uid FROM user WHERE email='%s' OR uname='%s' OR phone='%s'",
+    "SELECT email FROM user WHERE email='%s' OR uname='%s'",
     mysqli_real_escape_string($conn, $email),
-    mysqli_real_escape_string($conn, $uname),
-    $phone
+    mysqli_real_escape_string($conn, $uname)
 ); // generate query string with SQL injection protection
 $result = $conn->query($query);
-
-// only one match, but not the current user
-if ($result->num_rows == 1) {
-    if ($result->fetch_assoc()['uid'] != $_SESSION['uid']) {
+if ($result->num_rows === 0) {
+    $query = sprintf(
+        "UPDATE `user` SET `uname` = '%s', `name` = '%s', `email` = '%s', `phone` = '%s' WHERE `uid` = %s",
+        mysqli_real_escape_string($conn, $uname),
+        mysqli_real_escape_string($conn, $name),
+        mysqli_real_escape_string($conn, $email),
+        $phone,
+        $_SESSION['uid']
+    );
+    if ($conn->query($query) === TRUE) {
+        $obj = new stdClass();
+        $obj->success = true;
+        echo json_encode($obj);
+    } else {
         $obj = new stdClass();
         $obj->success = false;
-        $obj->error = "Username/email/phone already exists";
+        $obj->error = "Unknown error, please contact costomer service";
         echo json_encode($obj);
-        exit();
     }
-}
-
-// multiple match
-if ($result->num_rows > 1) {
+} else if ($result->num_rows >= 0) {
     $obj = new stdClass();
     $obj->success = false;
-    $obj->error = "Username/email/phone already exists";
-    echo json_encode($obj);
-    exit();
-}
-
-$query = sprintf(
-    "UPDATE `user` SET `uname` = '%s', `name` = '%s', `email` = '%s', `phone` = '%s' WHERE `uid` = %s",
-    mysqli_real_escape_string($conn, $uname),
-    mysqli_real_escape_string($conn, $name),
-    mysqli_real_escape_string($conn, $email),
-    $phone,
-    $_SESSION['uid']
-);
-if ($conn->query($query) === TRUE) {
-    $obj = new stdClass();
-    $obj->success = true;
+    $obj->error = "Email or username already exists";
     echo json_encode($obj);
 } else {
     $obj = new stdClass();
